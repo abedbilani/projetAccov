daf4498ee4e80030f76d85c61fc52d1b09ffb50c
Abed Bilani <Abed Bilani@AbedBilani-PC>
2017-08-03 01:24:19 +0300

    2nd commit : setting up communication sockets between client and server

--- src/src/Avion.java
+++ src/src/Avion.java
@@ -5,36 +5,118 @@
  */
 package src;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.PrintWriter;
+import java.io.Serializable;
 import java.net.Socket;
+import java.util.Scanner;
 
 /**
  *
  * @author Abed Bilani
  */
-public class Avion {
-    
-    
+public class Avion implements Serializable {
+
     // properties for avion
-    String name;
+    static String name;
     // plane coordonates
     int coordX;
     int coordY;
     int altittude;
-    //plane speed
+    //plane speed min speed and max speed
     int vitesseX;
     int vitesseY;
     int vitesseZ;
     // plane angle
     double cap;
-    
+
     //use socket to communicate
     static Socket socket;
-    
+
     //move plane in space
     //TODO
-    
     //change speed and cap
+    public Avion(String name) {
+
+        this.coordX = 0;
+        this.coordY = 0;
+        this.altittude = 0;
+        this.name = name;
+        this.cap = 0;
+        this.vitesseX = 0;
+        this.vitesseY = 0;
+        this.vitesseZ = 0;
+    }
+
+    void changeCoord(int x, int y, int z) {
+        if (x < 0) {
+            x = 0;
+        }
+        if (y < 0) {
+            y = 0;
+        }
+        if (z < 0) {
+            z = 0;
+        }
+        this.coordX = x;
+        this.coordY = y;
+        this.altittude = z;
+    }
+
+    void changeVitesse(int vx, int vy, int vz) {
+        if (vx < 0) {
+            vx = 0;
+        }
+        if (vy < 0) {
+            vy = 0;
+        }
+        if (vz < 0) {
+            vz = 0;
+        }
+
+        this.vitesseX = vx;
+        this.vitesseY = vy;
+        this.vitesseZ = vz;
+    }
+
+    void changeAngel(double angle) {
+
+        if (angle >= 0 && angle < 360) {
+            this.cap = angle;
+        }
+    }
+
+    Boolean avionConnect() {
+        try {
+            socket = new Socket("localhost", 1308);
+            return true;
+        } catch (IOException e) {
+            return false;
+        }
+    }
     
-    
-    
+    public String toString(){
+        return( Avion.this.name + " " + Avion.this.altittude );
+    }
+
+    public static void main(String[] args) {
+        System.out.println("lalalal2222222al");
+        try {
+            Scanner sc = new Scanner(System.in);
+            name = sc.nextLine();
+            Avion avion = new Avion(name);
+            avion.avionConnect();
+            ObjectOutputStream out = new ObjectOutputStream(avion.socket.getOutputStream());
+            ObjectInputStream in = new ObjectInputStream(avion.socket.getInputStream());
+            
+            out.writeObject(avion);
+            avion.socket.close();
+        } catch (Exception e) {
+        }
+    }
+
 }
--- src/src/Saca.java
+++ src/src/Saca.java
@@ -5,6 +5,16 @@
  */
 package src;
 
+import java.io.BufferedReader;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutput;
+import java.io.ObjectOutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
 import java.util.ArrayList;
 
 /**
@@ -12,12 +22,33 @@ import java.util.ArrayList;
  * @author Abed Bilani
  */
 public class Saca {
-    //save plane in a list
-    
-    public static ArrayList<Avion> list;
+//    //save plane in a list
+//
+//    public static ArrayList<Avion> list;
 
     // socket to communicate with planes
+    public static void main(String[] args) throws IOException {
+        System.out.println("lalalala1111111l");
+        try {
+
+            // creating sockets
+            ServerSocket ss = new ServerSocket(1308);
+            Socket s = ss.accept();
+            System.out.println("socket created");
+
+            // read client message 
+            ObjectInputStream dataIn = new ObjectInputStream(s.getInputStream());
+            ObjectOutputStream dataOut = new ObjectOutputStream(s.getOutputStream());
+            Avion avion = (Avion) dataIn.readObject();
+            System.out.println("data here");
+
+            System.out.println(avion.altittude);
+            s.close(); 
+       } catch (Exception e) {
+
+        }
+    }
     //socket to get commands 
     //socket to send data to the radar
-    
+
 }
